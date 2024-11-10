b6a9f8ad7c352a4155fd4e3b5df26fed8c161e08
Tommy Seternus <thomassseternus@gmail.com>
2024-11-09 20:53:51 -0500

    added system and system.IO

--- the-long-game-real-HW/the-long-game-real-HW/Program.cs
+++ the-long-game-real-HW/the-long-game-real-HW/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace the_long_game_real_HW
+﻿using System;
+using System.IO;
+
+namespace the_long_game_real_HW
 {
     internal class Program
     {
@@ -9,6 +12,10 @@
             string username = Console.ReadLine();
             int score = 0;
             //Console.WriteLine(username);
+
+            //Check if there is already an old file
+            string fileName = $"{username}.txt";
+
         }
     }
 }
